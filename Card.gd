2a54fde057a6a2d576f81e024bd1b7aef1ffc738
Charles Donovan <beam@berkeley.edu>
2024-08-19 21:11:44 -0700

    Tween and card deinstantiation.

--- Card.gd
+++ Card.gd
@@ -1,32 +1,34 @@
 extends Control
 var hover: bool = false
+@onready var scale_value: Vector2 = scale
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+	if hover && Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		hover = false
+		# Insert card function here or whateva
 		print("card triggers")
-
-
-func _on_texture_rect_mouse_exited():
-	material.set_shader_parameter("x_rot", 0)
-	material.set_shader_parameter("y_rot", 0)
+		var tween = create_tween()
+		tween.set_ease(Tween.EASE_OUT)
+		tween.set_trans(Tween.TRANS_EXPO)
+		tween.tween_property(self, "position", Vector2(position.x, position.y - 500), .5)
+		tween.tween_callback(get_parent().queue_free)
 
 
 func _on_gui_input(event):
 	if not event is InputEventMouseMotion: return
-	hover = true
 	
 	var angle_x_max = 15
 	var angle_y_max = 20
 	
 	var mouse_pos: Vector2 = get_local_mouse_position()
 	
-	# Normalize mouse position to the range [-1, 1] instead of [0, 1]
+	# Normalize mouse position to the range [-1, 1] 
 	var norm_x: float = (mouse_pos.x / size.x) * 2.0 - 1.0
 	var norm_y: float = (mouse_pos.y / size.y) * 2.0 - 1.0
 	
@@ -40,5 +42,18 @@ func _on_gui_input(event):
 
 
 func _on_mouse_exited():
+	hover = false
+	
+	# Reset attributes and shader parameters
+	position.y += 600
+	z_index = 0
+	scale = Vector2(scale_value.x, scale_value.y)
 	material.set_shader_parameter("x_rot", 0)
 	material.set_shader_parameter("y_rot", 0)
+
+
+func _on_mouse_entered():
+	hover = true
+	z_index = 1
+	scale = Vector2(scale_value.x + 3, scale_value.y + 3)
+	position.y -= 600
